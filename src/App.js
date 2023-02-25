7949dae5de81928d932db41f759b01f219dcbb2e
Harsha-Jagadish <harshajagadish99@gmail.com>
2023-02-24 23:22:37 -0800

    Deleted orginal src to manually create a new src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useState, useEffect} from 'react';
-
-
+import React from 'react';
 
 const App = () => {
 
-  const [counter, setCounter] = useState(0);
+    return(
+        <h1>App</h1>
 
-  useEffect(() => {
-  }, [counter]);
+    );
 
-
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount -1)}> - </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount +1)}> + </button>
-     
-    </div>
-  );
-};
+}
 
 export default App;
