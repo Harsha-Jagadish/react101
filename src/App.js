475b84db0fad1e55954b7314fa768574cd67daad
Harsha-Jagadish <harshajagadish99@gmail.com>
2023-02-24 23:42:58 -0800

    Imported search svg and app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import {useEffect} from 'react';
+
+//API Key: 422ee552
+
+const API_URL = 'http://www.omdbapi.com/?apikey=422ee552';
 
 const App = () => {
+
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+        
+    }, []);
 
     return(
         <h1>App</h1>
